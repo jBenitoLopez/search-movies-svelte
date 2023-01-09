14f7a8aecb7e2ad33ef39ca7d77ceeada004ddcc
cursoblv <107802557+jBenitoLopez@users.noreply.github.com>
2023-01-09 12:12:59 +0100

    add Card Component

---
+++ src/ExampleCard1.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Card from "./lib/Card.svelte";
+</script>
+
+<Card>
+  <p>Contenido de la card</p>
+  <footer slot="after">Pie de la card</footer>
+</Card>
+
+<Card>
+  <img
+    slot="image"
+    src="../src/assets/img_avatar2.png"
+    alt="Avatar"
+    style="width:100%"
+  />
+  <h2 slot="title">Título de la Card</h2>
+  <p>Nuevo Contenido de la card</p>
+  <footer slot="after">Pie de la card</footer>
+</Card>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,31 @@
+<article class="card">
+  <slot name="image"
+    ><img
+      src="../src/assets/img_avatar.png"
+      alt="Avatar"
+      style="width:100%"
+    /></slot
+  >
+  <header>
+    <slot name="title"><h3>Título por definir</h3></slot>
+  </header>
+  <div class="content">
+    <slot />
+  </div>
+  <slot name="after" />
+</article>
+
+<style>
+  .card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    transition: 0.3s;
+  }
+
+  .card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  }
+
+  .content {
+    padding: 2px 16px;
+  }
+</style>
