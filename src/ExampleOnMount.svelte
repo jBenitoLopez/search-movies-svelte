a7846842512c7d1d3768aeb8da3c8858575a6834
cursoblv <107802557+jBenitoLopez@users.noreply.github.com>
2023-01-09 17:15:59 +0100

    Example lifecycle on Mount

---
+++ src/ExampleOnMount.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from "svelte";
+
+  let data = [];
+
+  // Se ejecuta justo después de que el componente se haya montado en el DOM
+  onMount(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    data = await res.json();
+  });
+</script>
+
+<ul>
+  {#each data as item}
+    <li>{item.title}</li>
+  {/each}
+</ul>
