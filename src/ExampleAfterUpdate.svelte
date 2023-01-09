ecfd64199d1a089964bb25ef263a398817c36751
cursoblv <107802557+jBenitoLopez@users.noreply.github.com>
2023-01-09 17:15:40 +0100

    Example lifecycle after update

---
+++ src/ExampleAfterUpdate.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { afterUpdate } from "svelte";
+
+  let data = [
+    {
+      title: "Tarea 1",
+    },
+    {
+      title: "Tarea 2",
+    },
+    {
+      title: "Tarea 3",
+    },
+  ];
+
+  const addNew = () => {
+    data = [...data, { title: "Nueva tarea" }];
+  };
+
+  afterUpdate(() => {
+    // Hacemos scroll automático al último elemento del DOM
+    // si no está en el scroll
+    document.querySelector("ul").lastElementChild.scrollIntoView();
+  });
+</script>
+
+<button on:click={addNew}>Add new item</button>
+<ul>
+  {#each data as item}
+    <li>{item.title}</li>
+  {/each}
+</ul>
