4feb279bd90cb73bf022fc46fb5fd80f7e23c96b
cursoblv <107802557+jBenitoLopez@users.noreply.github.com>
2023-01-09 16:28:32 +0100

    Create Code component

---
+++ src/lib/Code.svelte
@@ -0,0 +1,18 @@
+<code>
+  <slot />
+</code>
+
+<style>
+  code {
+    font-size: 0.875rem;
+    line-height: 1.125rem;
+    text-indent: 0;
+    white-space: pre-wrap;
+    scrollbar-width: thin;
+    background: rgb(251, 251, 251);
+    color: rgb(92, 92, 92);
+    border: 1px solid rgb(235, 234, 234);
+    padding: 0.2rem;
+    margin: 0.2rem;
+  }
+</style>
